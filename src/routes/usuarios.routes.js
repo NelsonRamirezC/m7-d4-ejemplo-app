e9031e5515bf6e2851660382057c736f0cb9dadf
Nelson Ramirez <xhyskoax@gmail.com>
2023-07-08 11:47:28 -0400

    se incluye manejador de errores

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getAllUsuarios } from '../controllers/usuarios.controllers.js';
 const router = Router();
 
-router.get("/", getAllUsuarios);
 
+router.get("/", getAllUsuarios);
 
 export default router;
