c19248b8d0382d6ae412f8c712abe4b0f441d143
Nelson Ramirez <xhyskoax@gmail.com>
2023-07-07 21:45:09 -0400

    se configura express-hadleabrs y se crean vistas home y usuarios

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import usuariosRoutes from "./routes/usuarios.routes.js"
+import viewsRoutes from "./routes/views.routes.js";
+import { create } from "express-handlebars";
+
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
@@ -11,9 +17,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("tiny"));
 
+//configuración handlebars
+
+const hbs = create({
+    partialsDir: [path.resolve(__dirname, "./views/partials/")],
+});
+
+//configuración de handlebars como motor de plantillas
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "./views"));
+
+
 
 //endpoints
 app.use("/api/v1/usuarios", usuariosRoutes)
+
+//rutas de vistas
+app.use("/", viewsRoutes);
 
 
 //RUTAS NO DEFINIDAS
